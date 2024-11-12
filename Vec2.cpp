5cabf38931cc2bc4bcb851edad71bb497514a564
Max Sergueev <max.sergueev@artfx.fr>
2024-11-12 12:01:58 +0100

    ADD - Vector2

---
+++ Vec2.cpp
@@ -0,0 +1,4 @@
+#include "Vec2.h"
+
+const Vector2 Vector2::One{ 1, 1 };
+const Vector2 Vector2::Zero{ 0, 0 };
---
+++ Vec2.h
@@ -0,0 +1,62 @@
+#pragma once
+#include <cmath>
+struct Vector2 {
+	float x, y;
+
+	static const Vector2 Zero, One;
+
+	inline void operator+=(Vector2& right) {
+		x += right.x;
+		y += right.y;
+	}
+	inline void operator-=(Vector2& right) {
+		x -= right.x;
+		y -= right.y;
+	}
+
+	inline void operator*=(float scalar) {
+		x *= scalar;
+		y *= scalar;
+	}
+
+	inline void operator/=(float scalar) {
+		x /= scalar;
+		y /= scalar;
+	}
+
+	friend Vector2 operator+(Vector2& left, Vector2& right) {
+		return { left.x - right.x, left.y - right.y };
+	}
+
+	friend Vector2 operator*(Vector2& left, float scalar) {
+		return { left.x * scalar, left.y * scalar };
+	}
+
+	friend Vector2 operator*(float scalar, Vector2& right) {
+		return { scalar * right.x, scalar * right.y };
+	}
+
+	friend Vector2 operator/(Vector2& left, float scalar) {
+		return { left.x / scalar, left.y / scalar };
+	}
+
+	inline float GetMagnitude(){
+		return sqrt(x * x + y * y);
+	}
+
+	friend float Dot(Vector2& left, Vector2& right) {
+		return left.x * right.x + left.y * right.y;
+
+	}
+
+	inline float Normalize() {
+		(*this) /= GetMagnitude();
+	}
+
+	inline Vector2 Normalized() {
+		return (*this) / GetMagnitude();
+	}
+
+};
+
+
