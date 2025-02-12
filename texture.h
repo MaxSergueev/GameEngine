e81cb069be8c5cf785fe31a0025207e71cc28934
MaxSergueev <max.sergueev@artfx.fr>
2025-02-12 13:00:23 +0100

    [Feat] Math

---
+++ texture.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <cmath>
+#include <iostream>
+#include <string>
+
+#include "Renderer.h"
+
+class texture
+{
+	public:
+
+	protected: 
+
+	private:
+
+		const std::string mFileName;
+
+
+
+};
+
